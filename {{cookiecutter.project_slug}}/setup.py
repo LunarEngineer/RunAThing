1c37ea93fe77abc2141f4d2e362eafacd903c2c6
The Lunar Engineer <timothy.d.luna@gmail.com>
2023-11-20 21:33:24 +0000

    chore(pin): pin for testing

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,9 +1,9 @@
-${{ cookiecutter.template_components.header }}
+{{ cookiecutter.template_components.header }}
 
 from setuptools import setup
 
 
 setup(
     name="sample_project",
-    install_requires=${{ cookiecutter.project_dependencies.python.deps }}
+    install_requires={{ cookiecutter.project_dependencies.python.deps }}
 )
