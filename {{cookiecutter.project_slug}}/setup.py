79cff656d77a37c875422bd6e3fe81f3a5810e32
The Lunar Engineer <timothy.d.luna@gmail.com>
2023-11-20 21:32:46 +0000

    chore(pin): pin for testing

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,10 +1,9 @@
 ${{ cookiecutter.template_components.header }}
 
-# Simple test.
 from setuptools import setup
 
 
 setup(
     name="sample_project",
-    install_requires=['pyYaml>=6.0.1']
+    install_requires=${{ cookiecutter.project_dependencies.python.deps }}
 )
